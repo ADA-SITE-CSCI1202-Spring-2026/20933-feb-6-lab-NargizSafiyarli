cce1aca001837a26aec2166b9bb1531eeed2c5bc
NargizSafiyarli <nergizsefiyarli@gmail.com>
2026-02-06 13:54:59 +0400

    second easy exercise

--- src/PolarityOfNumber.java
+++ src/PolarityOfNumber.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class PolarityOfNumber {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+
+        if (x < 0) {
+            System.out.println(x + " is negative number");
+        } else if (x > 0) {
+            System.out.println(x + " is positive number");
+        } else System.out.println(x + " is neutral number");
+
 
     }
 }
